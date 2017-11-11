3f291d13d827039676b758a36b902503de4494a1
Mr. El <elanebritton96@gmail.com>
2017-11-11 13:31:23 -0500

    Changes

--- public_html/artScript.js
+++ public_html/artScript.js
@@ -1,1 +1,4 @@
-document.getElementById('art1').innerHTML = '<p>It works!</p>';
+function anime() {
+		document.getElementById('img1').innerHTML = '<img src="data:image/jpg;charset=utf8;base64, <?php $id = 1; echo $img; ?>" class="galleryImg"/>';
+		document.getElementById('title1').innerHTML = '<p id="t1"></p>';
+	}
--- public_html/getImage.php
+++ public_html/getImage.php
@@ -1,24 +1,13 @@
 <?php
-	/*$id = $_GET['id'];
+	$id = $_GET['id'];
 
 	$link = mysqli_connect("localhost", "mrel", "halo-2511", "el_gallery");
 	$sql = $link->query("select File from Anime where Image_ID = 1");
 	$row = mysqli_fetch_array($sql);
 
-	if (!$link) {
-		echo "<p>Unable to connect!</p>";
-	} else {
-		echo "<p>Connection to database succesfull!</p>";
-	}
+	$img = base64_encode($row['File']);
 
-	/*header("Content-type: image/jpeg");*/
-	/*echo $row['File'];
-
-
+	echo $img;
 	
-	mysqli_close($link);*/
-
-	echo "imgs/eziel.png";
-
-
+	mysqli_close($link);
 ?>
